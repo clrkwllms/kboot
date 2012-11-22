a86e650a4cd6e3f44302eb205965ec510283163a
Clark Williams <williams@redhat.com>
2012-11-22 11:53:10 -0600

    first try at handling both grub1 and grub2
    Signed-off-by: Clark Williams <williams@redhat.com>

--- kboot.py
+++ kboot.py
@@ -2,15 +2,45 @@
 
 import os
 import sys
+import os.path
 import subprocess
+
+def get_kernels(version):
+    kernels =  []
+    if version == 1:
+        for l in open('/etc/grub.conf'):
+            if l.startswith('title'):
+                kernels.append(l[6:-1])
+    else:
+        for l in open('/etc/grub2.cfg'):
+            if l.startswith('menuentry'):
+                kernels.append(l[11:l.find('--class') - 2])
+    return kernels
+
+def do_reboot(version, index):
+    if version == 1:
+        p = subprocess.Popen(["/sbin/grub", "--batch"], stdin=subprocess.PIPE)
+        p.stdin.write("savedefault --default=%d --once\n" % index)
+        p.stdin.close()
+        ret = p.wait()
+        if ret:
+            raise RuntimeError, "call to grub failed! (%d)" % ret
+    else:
+        #print "/sbin/grub2-reboot %d" % index
+        subprocess.call("/sbin/grub2-reboot %d" % index, shell=True)
+    subprocess.call("reboot", shell=True)
+
+#####################################################################
 
 if os.getuid() != 0:
     raise RuntimeError, "must be root!"
 
-kernel = []
-for l in open('/etc/grub.conf'):
-    if l.startswith('title'):
-        kernel.append("%s" % l[6:-1])
+if os.path.exists('/sbin/grub2-reboot'):
+    grub_version = 2
+else:
+    grub_version = 1
+
+kernel = get_kernels(grub_version)
 
 if len(sys.argv) <= 1:
     for i in range(0,len(kernel)):
@@ -22,11 +52,4 @@ else:
 if index < 0 or index >= len(kernel):
     raise ValueError, "invalid grub index %d (valid range: 0 - %d)" % (index, len(kernel)-1)
 
-p = subprocess.Popen(["/sbin/grub", "--batch"], stdin=subprocess.PIPE)
-p.stdin.write("savedefault --default=%d --once\n" % index)
-p.stdin.close()
-ret = p.wait()
-if ret:
-    raise RuntimeError, "call to grub failed! (%d)" % ret
-
-subprocess.call("reboot", shell=True)
+do_reboot(grub_version, index)
